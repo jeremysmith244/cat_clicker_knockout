c82928de2f911a3c5f58c06a31ae56d14784f82f
Jeremy Smith <jeremysmith244@gmail.com>
2021-04-08 20:37:24 -0700

    added nickname foreach databidning

--- js/app.js
+++ js/app.js
@@ -1,10 +1,35 @@
 var ViewModel = function() {
-    this.clickCount = ko.observable(0);
-    this.name = ko.observable('Tabby');
-    this.imgSrc = ko.observable('img/22252709_010df3379e_z.jpg');
 
-    this.incrementCounter = function() {
-        this.clickCount(this.clickCount() + 1);
+    self = this;
+    self.clickCount = ko.observable(0);
+    self.name = ko.observable('Tabby');
+    self.imgSrc = ko.observable('img/22252709_010df3379e_z.jpg');
+
+    self.nickNames = ko.observableArray([
+        {nick:'Tabitha'},
+        {nick:'Tabberella'},
+        {nick:'Tabytron Prime'},
+        {nick:'Holy Tabyrnacle'},
+    ]);
+
+    self.incrementCounter = function() {
+        self.clickCount(self.clickCount() + 1);
     };
+
+    self.level = ko.computed(function() {
+        if (self.clickCount() < 10) {
+            return 'Baby';
+        } else if (self.clickCount() < 20) {
+            return 'Teeny';
+        } else if (self.clickCount() < 30) {
+            return 'Meeny';
+        } else if (self.clickCount() < 40) {
+            return 'Goldy';
+        } else if (self.clickCount() < 60) {
+            return 'Oldy';
+        } else {
+            return 'Moldy';
+        };
+    });
 };
 ko.applyBindings(new ViewModel());
